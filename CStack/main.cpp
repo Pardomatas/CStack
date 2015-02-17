128d0b50a0f8419a498bb533f7efd336114ca99d
Mychal Wilson <mychal.wilson@gmail.com>
2015-02-17 12:00:09 -0500

    Testing
    Testing

--- CStack/main.cpp
+++ CStack/main.cpp
@@ -31,3 +31,28 @@ public:
         return !p;
     }
 };
+
+main()
+{
+    char c;
+    Stack acc(50);
+    int x;
+
+    while (cin.get(c))
+    {
+        x = 0;
+        while(c == ' ')
+            cin.get(c);
+        if(c == '+')
+            x = acc.pop() + acc.pop();
+        if(c == '*')
+            x = acc.pop() - acc.pop();
+        while(c >= '0' && c <= '9')
+        {
+            x = 10*x + (c-'0');
+            cin.get(c);
+        }
+        acc.push(x);
+    }
+    cout << acc.pop() << '\n';
+}
